cc66a2e2df4882b8891a2cf43bed635ec18c93f4
Thomas Baskin <tomcbaskin@gmail.com>
2022-04-13 14:38:04 -0600

    removed apikeys. oops!

--- client/components/chat_room/map.jsx
+++ client/components/chat_room/map.jsx
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
-import { Wrapper, Status } from '@googlemaps/react-wrapper';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
-// export class MapComp extends Component {
+
 export const MapComp = (props) => {
-  // export const MapComp = ({ lat, lng }) => {
   return (
     <div className="map">
-      {/* <script
-        async
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEJyBEm51-6F136X-7LHt3P6mzloabuTQ&callback=initMap"
-      ></script> */}
-      {/* <Wrapper apiKey={'AIzaSyCEJyBEm51-6F136X-7LHt3P6mzloabuTQ'} render={render}> */}
       <Map
         google={props.google}
         zoom={10}
@@ -19,20 +11,6 @@ export const MapComp = (props) => {
           lng: 10,
         }}
       ></Map>
-      {/* </Wrapper> */}
     </div>
   );
 };
-
-//export default MapComp;
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCEJyBEm51-6F136X-7LHt3P6mzloabuTQ',
-})(MapComp);
-
-// const render = (status: Status) => {
-//   return <h1>{status}</h1>;
-// };
-
-// <Wrapper apiKey={'AIzaSyCEJyBEm51-6F136X-7LHt3P6mzloabuTQ'} render={render}>
-//   <YourComponent />
-// </Wrapper>;
